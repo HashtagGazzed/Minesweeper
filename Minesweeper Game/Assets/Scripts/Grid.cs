e6714ed850550fd0dc483d89af15c6443cd1411c
Garry Morrison <gareth.p.morrison@hotmail.com>
2018-09-12 15:53:27 +1000

    Uploading finished iteration 1 for minesweeper game

---
+++ Minesweeper Game/Assets/Scripts/Grid.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+	public static int width = 11;
+	public static int height = 11;
+	public static Square[,] squares = new Square[width, height];
+
+	public static bool tntLocation(int x, int y) {
+		if (x < width && y < height && x >= 0 && y >= 0) {
+			return squares[x,y].tnt;
+		}
+		return false;
+	}
+
+	public static int adjacentTnt(int x, int y) {
+		int count = 0;
+		if (tntLocation(x, y + 1)) {
+			++count;
+		}
+		if (tntLocation(x + 1, y + 1)) {
+			++count;
+		}
+		if (tntLocation(x + 1, y)) {
+			++count;
+		}
+		if (tntLocation(x + 1, y - 1)) {
+			++count;
+		}
+		if (tntLocation(x, y - 1)) {
+			++count;
+		}
+		if (tntLocation(x - 1, y - 1)) {
+			++count;
+		}
+		if (tntLocation(x - 1, y)) {
+			++count;
+		}
+		if (tntLocation(x - 1, y + 1)) {
+			++count;
+		}
+		return count;
+	}
+
+	public static void revealTnt () {
+		foreach (Square square in squares) {
+			if (square.tnt) {
+				square.loadTexture(0);
+			}
+		}
+	}
+
+	public static void revealStack(int x, int y, bool[,] clicked) {
+		if (x < width && y < height && x >= 0 && y >= 0) {
+			if (clicked[x, y]) {
+				return;
+			}
+
+			squares[x, y].loadTexture(adjacentTnt(x, y));
+
+			if (adjacentTnt(x, y) > 0) {
+				return;
+			}
+
+			clicked[x, y] = true;
+			revealStack(x - 1, y, clicked);
+			revealStack(x + 1, y, clicked);
+			revealStack(x, y - 1, clicked);
+			revealStack(x, y + 1, clicked);
+		}
+	}
+
+	public static bool checkWin() {
+		foreach(Square square in squares) {
+			if (square.isGrass() && !square.tnt) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ Minesweeper Game/Assets/Scripts/Square.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour {
+
+	public bool tnt;
+
+	public Sprite[] emptyTextures;
+	public Sprite tntTexture;
+
+	void Start() {
+		tnt = Random.value < 0.15;
+		int x = (int)transform.position.x;
+		int y = (int)transform.position.y;
+		Grid.squares[x, y] = this;
+	}
+
+	public void loadTexture(int adjacentCount) {
+		if (tnt) {
+			GetComponent<SpriteRenderer>().sprite = tntTexture;
+		} else {
+			GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+		}
+	}
+
+	public bool isGrass() {
+		return GetComponent<SpriteRenderer>().sprite.texture.name == "master-tileset";
+	}
+
+	void OnMouseUpAsButton () {
+		if (tnt) {
+			Grid.revealTnt();
+			print("Game Over");
+		} else {
+			int x = (int)transform.position.x;
+			int y = (int)transform.position.y;
+			loadTexture(Grid.adjacentTnt(x, y));
+
+			Grid.revealStack(x, y, new bool[Grid.width, Grid.height]);
+
+			if (Grid.checkWin()) {
+				print("Congratulations, You Win!");
+			}
+		}
+	}
+
+}
